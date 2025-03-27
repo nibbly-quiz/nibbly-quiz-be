ce9cb60a664d7bfbb88b217655351cf327c9c320
libienz <leekh10195@gmail.com>
2025-03-27 19:46:09 +0900

    feat: DTO로부터 문제와 선지를 가져오는 기능 구현

--- src/main/java/com/nibbly/quiz/dto/QuizCreateRequest.java
+++ src/main/java/com/nibbly/quiz/dto/QuizCreateRequest.java
@@ -1,10 +1,25 @@
 package com.nibbly.quiz.dto;
 
+import com.nibbly.quiz.Question;
+import com.nibbly.quiz.domain.Option;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.util.List;
 
 public record QuizCreateRequest(
+        @NotNull(message = "질문은 비어있을 수 없습니다")
         QuestionCreateRequest questionRequest,
+        @NotNull(message = "선지는 비어있을 수 없습니다")
+        @Size(message = "문제당 선지는 최소 2개 이상, 10개 이하여야 합니다", min = 2, max = 10)
         List<OptionCreateRequest> optionCreateRequests
 ) {
+    public Question getQuestion() {
+        return questionRequest.toEntity();
+    }
+
+    public List<Option> getOptions(Long questionId) {
+        return optionCreateRequests.stream()
+                .map(optionCreateRequest -> optionCreateRequest.toEntity(questionId))
+                .toList();
+    }
 }
