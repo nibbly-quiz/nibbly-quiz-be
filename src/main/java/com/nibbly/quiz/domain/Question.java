cf1e6cb4aff4c5f46f67038307a5c4837ebfa7bc
libienz <leekh10195@gmail.com>
2025-03-27 14:43:57 +0900

    feat: 문제 도메인 설계 및 구현

---
+++ src/main/java/com/nibbly/quiz/domain/Question.java
@@ -0,0 +1,34 @@
+package com.nibbly.quiz;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDate;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Question {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String text;
+
+    @Column(nullable = false)
+    private LocalDate scheduleAt;
+
+    public Question(Long id, String text, LocalDate scheduleAt) {
+        this.id = id;
+        this.text = text;
+        this.scheduleAt = scheduleAt;
+    }
+}
