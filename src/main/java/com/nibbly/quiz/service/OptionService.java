f4018b2632594d12fa24d97090edf69a1d26b72a
libienz <leekh10195@gmail.com>
2025-03-28 02:16:55 +0900

    fix: 서비스 메서드 트랜잭셔널 누락 수정

--- src/main/java/com/nibbly/quiz/service/OptionService.java
+++ src/main/java/com/nibbly/quiz/service/OptionService.java
@@ -7,6 +7,7 @@ import com.nibbly.quiz.global.exception.NibblyQuizException;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,6 +15,7 @@ public class OptionService {
 
     private final OptionRepository optionRepository;
 
+    @Transactional
     public void saveOptions(List<Option> options) {
         validateHasAnswer(options);
         validateNoDuplication(options);
--- src/main/java/com/nibbly/quiz/service/QuestionService.java
+++ src/main/java/com/nibbly/quiz/service/QuestionService.java
@@ -7,6 +7,7 @@ import com.nibbly.quiz.global.exception.NibblyQuizException;
 import java.time.LocalDate;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,6 +15,7 @@ public class QuestionService {
 
     private final QuestionRepository questionRepository;
 
+    @Transactional
     public Long saveQuestion(Question question) {
         validateScheduleDate(question);
         return questionRepository.save(question).getId();
