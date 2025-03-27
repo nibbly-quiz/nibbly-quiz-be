2cc1a28d537a699f62c733cf1632e668a6a45a4f
libienz <leekh10195@gmail.com>
2025-03-27 14:44:24 +0900

    feat: 선지 도메인 설계 및 구현

---
+++ src/main/java/com/nibbly/quiz/domain/Option.java
@@ -0,0 +1,37 @@
+package com.nibbly.quiz.domain;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Option {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long questionId;
+
+    @Column(nullable = false)
+    private String text;
+
+    @Column(nullable = false)
+    private boolean isAnswer;
+
+    public Option(Long id, Long questionId, String text, boolean isAnswer) {
+        this.id = id;
+        this.questionId = questionId;
+        this.text = text;
+        this.isAnswer = isAnswer;
+    }
+}
