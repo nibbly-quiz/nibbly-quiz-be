6499a9273740c32083be296a2fd628073852ac1f
libienz <leekh10195@gmail.com>
2025-04-01 00:20:36 +0900

    style: ErrorCode 순서 가독성 조정

--- src/main/java/com/nibbly/global/exception/ErrorCode.java
+++ src/main/java/com/nibbly/global/exception/ErrorCode.java
@@ -13,10 +13,10 @@ public enum ErrorCode {
     NO_CORRECT_ANSWER("정답이 없는 문제는 등록할 수 없습니다", HttpStatus.BAD_REQUEST),
     INVALID_OPTION_LENGTH("선지 내용은 100자를 넘을 수 없습니다", HttpStatus.BAD_REQUEST),
     INVALID_QUESTION_LENGTH("문제 내용은 500자를 넘을 수 없습니다", HttpStatus.BAD_REQUEST),
-
-    INTERNAL_SERVER_ERROR("서버 내부 오류가 발생하였습니다", HttpStatus.INTERNAL_SERVER_ERROR),
     INVALID_OPTION_SIZE("선지는 2개 이상 10개 이하로 등록해야 합니다", HttpStatus.BAD_REQUEST),
     QUESTION_NOT_FOUND("문제를 찾을 수 없습니다", HttpStatus.NOT_FOUND),
+    
+    INTERNAL_SERVER_ERROR("서버 내부 오류가 발생하였습니다", HttpStatus.INTERNAL_SERVER_ERROR),
     ;
 
     private final String message;
