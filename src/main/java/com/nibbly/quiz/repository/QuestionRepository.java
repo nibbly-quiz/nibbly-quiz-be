cd26e0cdeb441e077469a70ceb67f4881dba1f77
libienz <leekh10195@gmail.com>
2025-03-31 21:41:38 +0900

    refactor: 오늘의 문제 조회를 ID만 조회하도록 개선

--- src/main/java/com/nibbly/quiz/repository/QuestionRepository.java
+++ src/main/java/com/nibbly/quiz/repository/QuestionRepository.java
@@ -4,9 +4,12 @@ import com.nibbly.quiz.Question;
 import java.time.LocalDate;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface QuestionRepository extends JpaRepository<Question, Long> {
-    List<Question> findByScheduledAt(LocalDate scheduleAt);
+    @Query("SELECT q.id FROM Question q WHERE q.scheduledAt = :scheduleAt")
+    List<Long> findByScheduledAt(@Param("scheduleAt") LocalDate scheduleAt);
 }
