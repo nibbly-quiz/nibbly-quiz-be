9d5c60a4cb413b7cded92f6141d71b9950588eb9
libienz <leekh10195@gmail.com>
2025-04-02 17:36:48 +0900

    test: QuestionServiceTest Fixture 기반으로 개선

--- src/test/java/com/nibbly/quiz/service/QuestionServiceTest.java
+++ src/test/java/com/nibbly/quiz/service/QuestionServiceTest.java
@@ -7,6 +7,7 @@ import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import com.nibbly.global.exception.NibblyQuizException;
 import com.nibbly.global.supports.DatabaseCleaner;
 import com.nibbly.quiz.Question;
+import com.nibbly.quiz.fixture.QuizFixture;
 import com.nibbly.quiz.repository.QuestionRepository;
 import java.time.LocalDate;
 import java.util.List;
@@ -36,7 +37,7 @@ class QuestionServiceTest {
     @Test
     void should_create_question() {
         // given
-        Question question = new Question("정상적인 문제", LocalDate.now().plusDays(1));
+        Question question = QuizFixture.QUIZ.getQuestion();
 
         // when & then
         assertThatCode(() -> questionService.saveQuestion(question))
@@ -47,10 +48,10 @@ class QuestionServiceTest {
     @Test
     void should_throw_exception_when_question_is_scheduled_before_today() {
         // given
-        Question question = new Question("출제일이 어제인 문제", LocalDate.now().minusDays(1));
+        Question yesterdayQuestion = QuizFixture.QUIZ.getQuestionScheduledAt(LocalDate.now().minusDays(1));
 
         // when & then
-        assertThatThrownBy(() -> questionService.saveQuestion(question))
+        assertThatThrownBy(() -> questionService.saveQuestion(yesterdayQuestion))
                 .isInstanceOf(NibblyQuizException.class)
                 .hasMessage("과거 일자에 대한 문제는 등록할 수 없습니다");
     }
@@ -59,25 +60,21 @@ class QuestionServiceTest {
     @Test
     void should_find_question_ids_scheduled_today() {
         // given
-        Question question1 = new Question("오늘 출제될 문제1", LocalDate.now());
-        Question question2 = new Question("오늘 출제될 문제2", LocalDate.now());
-        Question question3 = new Question("내일 출제될 문제", LocalDate.now().plusDays(1));
-        questionRepository.save(question1);
-        questionRepository.save(question2);
-        questionRepository.save(question3);
+        Question todayQuestion = QuizFixture.QUIZ.getQuestionScheduledAt(LocalDate.now());
+        questionRepository.save(todayQuestion);
 
         // when
         List<Long> questionIds = questionService.readQuestionIdsScheduledToday();
 
         // then
-        assertThat(questionIds).containsExactlyInAnyOrder(question1.getId(), question2.getId());
+        assertThat(questionIds).containsExactly(todayQuestion.getId());
     }
 
     @DisplayName("문제 ID로 문제를 조회할 수 있다.")
     @Test
     void should_find_question_by_id() {
         // given
-        Question question = new Question("문제", LocalDate.now());
+        Question question = QuizFixture.QUIZ.getQuestion();
         Long questionId = questionRepository.save(question).getId();
 
         // when
@@ -92,10 +89,10 @@ class QuestionServiceTest {
     @Test
     void should_throw_exception_when_question_not_found() {
         // given
-        Long questionId = 1L;
+        Long noExistQuestionId = 1L;
 
         // when & then
-        assertThatThrownBy(() -> questionService.readQuestion(questionId))
+        assertThatThrownBy(() -> questionService.readQuestion(noExistQuestionId))
                 .isInstanceOf(NibblyQuizException.class)
                 .hasMessage("문제를 찾을 수 없습니다");
     }
