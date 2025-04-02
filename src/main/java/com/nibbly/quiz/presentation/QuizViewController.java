fe4b73f61e8f5f685ab9e7b3b033574a793b8f14
libienz <leekh10195@gmail.com>
2025-04-02 21:49:53 +0900

    feat: 오늘의 문제 view 구현

--- src/main/java/com/nibbly/quiz/presentation/QuizViewController.java
+++ src/main/java/com/nibbly/quiz/presentation/QuizViewController.java
@@ -1,14 +1,18 @@
 package com.nibbly.quiz.presentation;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class QuizViewController {
 
     @GetMapping("/quiz/new")
-    public String newQuiz(Model model) {
+    public String newQuiz() {
         return "quiz-form";
+    }
+
+    @GetMapping("/quiz/today")
+    public String solveQuiz() {
+        return "quiz-today";
     }
 }
