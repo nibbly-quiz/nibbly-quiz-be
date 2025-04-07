3b54296a1714f61efc7e24832e9f89e3659b548d
libienz <leekh10195@gmail.com>
2025-04-07 16:43:34 +0900

    feat: 문제 ID 목록으로 정답 선지 목록을 조회하는 Repository 기능 구현

--- src/main/java/com/nibbly/quiz/repository/OptionRepository.java
+++ src/main/java/com/nibbly/quiz/repository/OptionRepository.java
@@ -3,9 +3,14 @@ package com.nibbly.quiz.repository;
 import com.nibbly.quiz.domain.Option;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OptionRepository extends JpaRepository<Option, Long> {
     List<Option> findByQuizId(Long quizId);
+
+    @Query("SELECT o FROM Option o WHERE o.quizId IN :quizIds AND o.isAnswer = true")
+    List<Option> findAnswerOptions(@Param("quizIds") List<Long> quizIds);
 }
