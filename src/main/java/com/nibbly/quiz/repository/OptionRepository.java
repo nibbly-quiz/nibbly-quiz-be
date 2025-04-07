3373ddb736466f69c4fe125f034eeb567cf7e3b7
libienz <leekh10195@gmail.com>
2025-04-07 17:50:35 +0900

    refactor: 순서가 필요 없는 퀴즈 ID 파라미터 Set으로 타입 변환

--- src/main/java/com/nibbly/quiz/repository/OptionRepository.java
+++ src/main/java/com/nibbly/quiz/repository/OptionRepository.java
@@ -2,6 +2,7 @@ package com.nibbly.quiz.repository;
 
 import com.nibbly.quiz.domain.Option;
 import java.util.List;
+import java.util.Set;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,5 +13,5 @@ public interface OptionRepository extends JpaRepository<Option, Long> {
     List<Option> findByQuizId(Long quizId);
 
     @Query("SELECT o FROM Option o WHERE o.quizId IN :quizIds AND o.isAnswer = true")
-    List<Option> findAnswerOptions(@Param("quizIds") List<Long> quizIds);
+    List<Option> findAnswerOptions(@Param("quizIds") Set<Long> quizIds);
 }
